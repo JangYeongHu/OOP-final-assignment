c8e10b31492ca2d0c2d8edd6d384a5c144c9e3b9
sdd5 <rudwo6285@gmail.com>
2024-11-12 22:58:07 +0900

    대장장이클래스추가 검만드는역할 게임스크린 이미지 추가 검클래스에서 가격,판매가,강화, getname등 필드얻을수있는 매서드추가
    게임스크린은 아직더만들어야합니다
    그리고 검은 어디서만들게 하는게 좋을까요?
    일단 대장장이클래스에서 만들게 했는데 게임스크린에서 만드는게 좋을까요?

--- src/item/Sword.java
+++ src/item/Sword.java
@@ -1,12 +1,19 @@
 package src.item;
 
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
 public class Sword {
-    private int getPrice;
 
-    private int sellPrice;
-    private int possibility;
+    private int upgradeFee;//강화비용
+    private int sellPrice;//판매가
+    private int possibility;//강화도
 
-
+    //성공횟수,실패횟수,시도횟수
+    private int SuccessCount;
+    private int getFailureCount;
+    private int getTotalAttempts;
 
     private String imageSourcePath;
 
@@ -17,4 +24,63 @@ public class Sword {
     public String upGradeSword() {
         return "Success";
     }
+    boolean upgrade_probability(int U) {// 현재 강화율과 비교해서 강화확률조정
+        int num = 100 - 5*possibility;// 성공확률계산
+        Random rand = new Random();
+        int n = rand.nextInt(1,101);// 성공
+        if(n > num){
+            return false;
+        }
+        return true;
+    }
+
+    public JLabel imageLabel(){
+        ImageIcon imageicon = new ImageIcon(imageSourcePath);//
+        Image image = imageicon.getImage();
+        int newWidth = 400;
+        int newHeight = 400;
+        Image resizedImage = image.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
+        ImageIcon resizedIcon = new ImageIcon(resizedImage);
+        return new JLabel(resizedIcon);
+    }
+
+
+    public Sword (String n, String d,String img){//검만들기 검이름과 이미지파일 이름을넣는다
+        name = n;
+        description = d;
+        imageSourcePath = img;
+        possibility = 0;
+    }
+    public void priceSetting(){//강화도비례 특정강화도 이상부터 판매가와 강화비용설정
+        upgradeFee = 100*possibility;
+        if (possibility > 4)
+            sellPrice = 200*possibility;
+        if (possibility > 8)
+            sellPrice = 400*possibility;
+        if (possibility > 12)
+            sellPrice = 800*possibility;
+        if (possibility > 16)
+            sellPrice = 1600*possibility;
+        else
+            sellPrice = 110*possibility;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+    public int SuccessCount(){
+        return SuccessCount;
+    }
+    public int getFailureCount(){
+        return getFailureCount;
+    }
+    public int getTotalAttempts(){
+        return getTotalAttempts;
+    }
+    public int getSuccessRate(){//성공비율
+        return 0;
+    }
+
 }
--- src/screen/GameScreen.java
+++ src/screen/GameScreen.java
@@ -1,20 +1,62 @@
 package src.screen;
 
+import src.item.Sword;
+import src.player.Player;
 import src.screen.interfaces.Screen;
 import src.main.MainController;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
 
 public class GameScreen extends JPanel implements Screen {
-
+    ArrayList<Sword> Slist = new ArrayList<>();
+    Player player = new Player();
+    JLabel money = new JLabel("돈 : "+player.getMoney());
     private MainController mainController;
     public GameScreen(MainController mainController) {
         this.mainController = mainController;
         initialize();
     }
 
+    void topPanel(){
+        JPanel top = new JPanel(new GridLayout(1, 3));
+        top.setPreferredSize(new Dimension(1200, 50));
+
+        top.setBackground(Color.green);
+        button(top);
+        add(top, BorderLayout.PAGE_START);
+    }
+
+    void button(Container j){
+        JButton MainButton = new JButton("메인으로 돌아가기");
+        JButton SaveButton = new JButton("저장하기");
+        j.add(MainButton, BorderLayout.LINE_START);
+        j.add(money, BorderLayout.CENTER);
+        money.setFont(money.getFont().deriveFont(24.0f));
+        j.add(SaveButton, BorderLayout.LINE_END);
+    }
+    void midPanel(){
+        JPanel mid = new JPanel(new GridLayout(1,2));
+
+
+        JButton SwordSave = new JButton("파괴방지권 사용");
+        SwordSave.setPreferredSize(new Dimension(100,100));
+        add(SwordSave);
+
+        add(mid, BorderLayout.CENTER);
+    }
+    void midbutton(Container c){//색깔로 사용유무확인
+    }
+
+    void bottomPanel(){
+
+    }
+
     @Override
     public void initialize() {
+        topPanel();
+        midPanel();
     }
 
     @Override
@@ -26,5 +68,20 @@ public class GameScreen extends JPanel implements Screen {
     public void hideScreen() {
         setVisible(false);
     }
+}
+class Forges {
+    //미리 검을 만드는 방법 vs 검을 강화를 성공을 하면 만드는것 임시로 검만드는 위치를 여기로했습니다
+    //검이름과 설명을 읽는 작업이 필요함
+    //검이름, 검의 이미지 임시로 이름을 1,2,---,20으로 정함 !!임시!!
+    //대장간안에 검이 있다는 느낌으로 여기있는 검을 움직일것
+    static ArrayList<Sword> Slist = new ArrayList<>();
+    static void CreateSword(){
+        for (int i = 1; i < 21; i++){
+            Slist.add(new Sword(i+"",i+"번째 검이다","res/"+i+".png"));
+        }
+    }
 
+    String imageSword(int i){
+        return "";
+    }
 }
