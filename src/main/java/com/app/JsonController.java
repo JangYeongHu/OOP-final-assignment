2b40798f6839c07787558d02af6dc8e7d61cd0df
Hyrge <ekek7478@gmail.com>
2024-11-24 07:53:13 +0900

    통계 sava, load 구현

--- src/main/java/com/app/JsonController.java
+++ src/main/java/com/app/JsonController.java
@@ -11,20 +11,16 @@ import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-
 import java.nio.file.Files;
 import java.nio.file.Paths;
-
+import java.util.ArrayList;
 
 
 
 public class JsonController {
 
     private static JsonController singletonJsonController;
-    private JSONArray statisticsData;
     private static final String FILE_PATH = "src/main/resources/savefile.json";
-
-    private JSONArray logData;
     private JSONArray rowDatas;
 
     //싱글톤으로 관리해주는 메소드
@@ -44,7 +40,7 @@ public class JsonController {
 
 
 
-
+    //Json 읽기
     public void readJson() {
         try {
             String jsonText = new String(Files.readAllBytes(Paths.get(FILE_PATH)), StandardCharsets.UTF_8);
@@ -58,46 +54,35 @@ public class JsonController {
 
     //로드 클릭 시 해당 인덱스의 데이터를 가져오는 메소드
     public void loadJsonDataWithIndex(int index) {
-
         JSONObject jsonData = rowDatas.getJSONObject(index);
-        loadPlayerDataInJson(jsonData, index);
-        //어쩌면 통계와 로그 데이터도 싸그리 플레이어 클래스에서 관리해야 할 수 있습니다.
-        loadStatisticsDataInJson(jsonData);
-        loadLogDataInJson(jsonData);
-
-        loadPlayerDataInJson(rowDatas.getJSONObject(index), index);
-        loadStatisticsDataInJson(rowDatas.getJSONObject(index));
-    }
-
-
-    //파일에서 플레이어 데이터를 읽어와 저장하는 메소드
-    private void loadPlayerDataInJson(JSONObject jsonData,int index) {
-        JSONObject playerData = jsonData.getJSONObject("player_data");
-        setPlayerData(playerData,index);
-    }
-
-    //파일에서 통계 데이터를 읽어와 저장하는 메소드
-    private void loadStatisticsDataInJson(JSONObject jsonData) {
-
-        statisticsData = jsonData.getJSONArray("statistics");
-    }
-
-    //파일에서 로그 데이터를 읽어와 저장하는 메소드
-    private void loadLogDataInJson(JSONObject jsonData) {
-        logData = jsonData.getJSONArray("log");
+        setPlayerData(jsonData,index);
     }
 
 
     //플레이어 데이터 설정하기
-    private void setPlayerData(JSONObject playerData,int index) {
+    private void setPlayerData(JSONObject jsonData,int index) {
+        //player 데어터 관련
+        JSONObject playerData = jsonData.getJSONObject("player_data");
         Player player = Player.getInstance(index);
         player.setMoney(playerData.getInt("money"));
         player.setNowSword(MainController.findSwordById(playerData.getInt("sword_id")));
         player.setUpdatedDate(playerData.getString("latest_date"));
 
-        System.out.println("setPlayerData" + player.getUpdateDate());
+        //statistics 관련
+        ArrayList<int[]> statics = new ArrayList<>(); // [SuccessCount,FailureCount]
+        JSONArray statisticsData = jsonData.getJSONArray("statistics");
+        for (int i = 0; i < statisticsData.length() ; i++) {
+            JSONObject jo = statisticsData.getJSONObject(i);
+            int sword_id = jo.getInt("sword_id");
+            int data[] = {jo.getInt("success"), jo.getInt("fail")};
+            statics.add(sword_id-1, data);
+        }
+        player.setStatics(statics);
+
+//        System.out.println("setPlayerData" + player.getUpdateDate());
         //item 관련
-        
+
+        //log 관련
     }
 
 
@@ -118,17 +103,22 @@ public class JsonController {
         String formattedDateTime = currentDateTime.format(formatter);
 
         playerData.put("latest_date", formattedDateTime);
-
-
         rowDatas.getJSONObject(index).put("player_data", playerData);
 
-//        System.out.println(rowDatas.toString());
-
-
-
+        // 통계 정보 가져오기 
+        JSONArray statistics = new JSONArray();
+        ArrayList<int[]> playerStatics = player.getStatics();
+        for (int i = 0; i < playerStatics.size(); i++) {
+           JSONObject jo = new JSONObject();
+           jo.put("sword_id", i+1);
+           jo.put("success", playerStatics.get(i)[0]);
+           jo.put("fail", playerStatics.get(i)[1]);
+           statistics.put(jo);
+        }
+        rowDatas.getJSONObject(index).put("statistics", statistics);
     }
 
-    //파일 처리 분리
+    //파일 처리 분리 - 쓰기
     public void writeJson(int index) {
         savaPlayerData(index);
         try {
@@ -147,27 +137,6 @@ public class JsonController {
         readJson();
         loadJsonDataWithIndex(index);
     }
-
-
-
-    /* 클래스를 통으로 재설계하는 과정에서 불가피하게 빠졌습니다. 어떻게 잘 수정해서 쓰고 싶은데 지금은 시간이 없어서 보류하겠습니다.
-    public List<Map<String, Integer>> getInfo(int keyValue) { //key - 0은 statistics, key - 1은 log
-        //2차원 배열로 받길 원하시면 수정하겠습니다
-
-        List<Map<String, Integer>> mapList = new ArrayList<>();
-        JSONArray ja = user.getJSONArray(lableKeys[keyValue]);
-
-        for (int i = 0; i < ja.length(); i++) {
-            JSONObject jsonObject = ja.getJSONObject(i);
-            Map<String, Integer> map = new HashMap<>();
-            jsonObject.keys().forEachRemaining(key -> map.put(key, jsonObject.getInt(key)));
-            mapList.add(map);
-        }
-        mapList.forEach(System.out::println);
-
-        return mapList;
-    }
-    */
 
 
 //    public static void main(String[] args) {
